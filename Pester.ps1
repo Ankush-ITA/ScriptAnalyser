839967d63ba13b875d7295945aa0491f3e82a356
Ankush-ITA <127494609+Ankush-ITA@users.noreply.github.com>
2025-03-11 23:37:18 +0530

    Add files via upload

---
+++ Pester.ps1
@@ -0,0 +1,23 @@
+﻿# MyScriptWithTests.ps1
+
+# Function Definition
+function Add-Numbers {
+    param (
+        [int]$a,
+        [int]$b
+    )
+    return $a + $b
+}
+
+# Pester Tests
+Describe "Add-Numbers" {
+    It "Should add two numbers correctly" {
+        $result = Add-Numbers -a 2 -b 3
+        $result | Should -Be 5
+    }
+}
+
+# Run the tests
+Invoke-Pester -ScriptBlock {
+    . $MyInvocation.MyCommand.Path
+}
